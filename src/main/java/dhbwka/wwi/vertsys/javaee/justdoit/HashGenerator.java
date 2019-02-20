13b8ad4b41778564ab8524e6266be3fd7f39525f
DerStimmler <43130816+DerStimmler@users.noreply.github.com>
2019-02-20 12:54:49 +0100

    implemented HashGenerator with SHA-256 algorithm

---
+++ src/main/java/dhbwka/wwi/vertsys/javaee/justdoit/HashGenerator.java
@@ -0,0 +1,31 @@
+package dhbwka.wwi.vertsys.javaee.justdoit;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import javax.ejb.Stateless;
+
+
+@Stateless
+public class HashGenerator {
+
+    public HashGenerator() {
+        // EJB need this one.
+    }
+
+    String algorithm = "SHA-256";
+    
+    public String getHashText(String text) {
+
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(this.algorithm);
+            byte[] hash = messageDigest.digest(text.getBytes(StandardCharsets.UTF_8));
+            String encoded = Base64.getEncoder().encodeToString(hash);
+
+            return encoded;
+        } catch (NoSuchAlgorithmException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
