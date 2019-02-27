a0de0750d0824f08812491736445cf451e415731
skylinersandre <43538922+skylinersandre@users.noreply.github.com>
2019-02-27 20:31:18 +0100

    edited email check with regex for registration and change email

--- src/main/java/justdoit/common/ValidationBean.java
+++ src/main/java/justdoit/common/ValidationBean.java
@@ -7,6 +7,7 @@ import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
+import justdoit.user.User;
 
 /**
  * This bean validates an entity before saving the entity to the database. (cf.
@@ -18,6 +19,8 @@ public class ValidationBean {
 
     @Resource
     Validator validator;
+
+    private final String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])";
 
     /**
      * This method validates an entity and returns a new List of errors.
@@ -46,6 +49,14 @@ public class ValidationBean {
         violations.forEach((ConstraintViolation<T> violation) -> {
             errors.add(violation.getMessage());
         });
+
+        if (object instanceof User) {
+            User user = (User) object;
+            String email = user.getEmail();
+            if (!email.matches(regex)) {
+                errors.add("Bitte gib eine gültige E-Mail-Adresse ein");
+            }
+        }
 
         return errors;
     }
