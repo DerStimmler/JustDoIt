bf63898d8df81dbf174eea5d4df582f881454e2d
AnsgarLichter <ansgar.lichter@dh-karlsruhe.de>
2019-02-27 13:14:51 +0100

    Added the possibility to assign todos to other users

--- src/main/java/justdoit/servlet/CreateToDoServlet.java
+++ src/main/java/justdoit/servlet/CreateToDoServlet.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.sql.Date;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.ejb.EJBException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import justdoit.common.FormatUtils;
 import justdoit.common.ValidationBean;
+import justdoit.exceptions.EntityAlreadyExistsException;
 import justdoit.task.bean.CategoryBean;
 import justdoit.task.bean.ToDoBean;
 import justdoit.task.entitiy.Category;
@@ -76,18 +78,27 @@ public class CreateToDoServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         List<User> user = new ArrayList<>();
         List<String> errors = new ArrayList<>();
         HttpSession session = request.getSession();
 
-        //Check User
         User todoUser = this.userBean.findByUsername(request.getParameter("todo_user"));
         user.add(todoUser);
-        //Check Category
+
         CategoryId id = new CategoryId(request.getParameter("todo_user"), request.getParameter("todo_category"));
         Category todoCategory = this.categoryBean.findById(id);
-
+        if (todoUser != this.userBean.getCurrentUser() && todoCategory == null) {
+            try {
+                this.categoryBean.saveNew(todoCategory, id);
+            } catch (EJBException ex) {
+                if(ex.getCausedByException() instanceof EntityAlreadyExistsException) {
+                    errors.add("Das ToDo kann dem Benutzer $user nicht unter der Kategorie $category zugewiesen werden"
+                               .replace("$user", todoUser.getUsername())
+                               .replace("$category", todoCategory.getCategoryName()));
+                }
+            }
+        }
         //Parse Date
         Date dueDate = FormatUtils.parseDate(request.getParameter("todo_due_date"));
         //Parse Time
