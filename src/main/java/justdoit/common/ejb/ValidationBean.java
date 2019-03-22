47e5c78ab9bcf352744baeff54c283468a03ff8e
AnsgarLichter <ansgar.lichter@dh-karlsruhe.de>
2019-03-22 11:22:56 +0100

    ValidationBean: replaced regex

--- src/main/java/justdoit/common/ejb/ValidationBean.java
+++ src/main/java/justdoit/common/ejb/ValidationBean.java
@@ -10,9 +10,7 @@ import javax.validation.Validator;
 import justdoit.common.jpa.User;
 
 /**
- * This bean validates an entity before saving the entity to the database. (cf.
- * https://www.baeldung.com/javax-validation).
- *
+ * This bean validates an entity before saving the entity to the database.
  */
 @Stateless
 public class ValidationBean {
@@ -20,7 +18,7 @@ public class ValidationBean {
     @Resource
     Validator validator;
 
-    private final String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])";
+    private final String regex = "\\S+@\\S+.[a-z]{2}";
 
     /**
      * This method validates an entity and returns a new List of errors.
